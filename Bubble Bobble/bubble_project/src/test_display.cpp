2d79d161c7c4f70868cfceb9130f354daa18cdbf
Angel Kyriakopoulos <angelkyriako@gmail.com>
2013-01-12 18:09:51 +0200

    Revert "test reading complete"
    This reverts commit 4ce1987be2467197e915e471536153498c2780ac.

--- Bubble Bobble/bubble_project/src/test_display.cpp
+++ Bubble Bobble/bubble_project/src/test_display.cpp
@@ -1,81 +1,72 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
-#include <allegro5/allegro.h>
+#include <string>
 
-const int tile_size = 8;
+#include <allegro5\allegro.h>
+#include <allegro5\allegro_native_dialog.h>
 
-const char* TERRAIN__DATA_PATH = "..\\data\\terrain_data";
-const char*	STAGE1_PATH = "..\\data\\terrain_data\\stage1.txt";
-const char* TILE_BITMAP_PATH = "..\\data\\terrain_data\\tile_bitmap.png";
+#define TILE_SIZE 8
+#define TERRAIN__DATA_PATH "..\data\terrain_data"
+#define STAGE1 "stage1.txt"
+#define TILE_BITMAP "tile_bitmap.png"
 
 const int SCREEN_W = 800;
 const int SCREEN_H = 600;
 unsigned char *terrain;
 unsigned int terrain_x, terrain_y;
 
-
-unsigned char get_terrain_tile(int row, int col){
-	return terrain[row+col*terrain_x];
+unsigned int get_terrain_tile(int row, int col){
+	return terrain[row+col*terrain_y];
 }
 
-void Load_Terrain_Text(std::string filename){
+void Load_Terrain(std::string filename){
 	unsigned int load_counter_x = 0, load_counter_y = 0;
 
 	std::ifstream openfile(filename);
 	if (openfile.is_open()){
 		openfile >> terrain_x >> terrain_y;
+		std::putchar(terrain_x);
+		std::putchar('\t');
 
-		terrain = new unsigned char [terrain_x*terrain_y];
-		for (int i=0; i<terrain_y; ++i){
-			for (int j=0; j<terrain_x; ++j){
-				openfile >> terrain[i+j*terrain_x];
-			}
-		}
+		terrain = new unsigned char[terrain_x * terrain_y];
+		for (int i=0; i<terrain_x; ++i)
+			for (int j=0; j<terrain_y; ++j)
+				openfile >> terrain[i*j];
 		
 	}
+
 }
 
-void Draw_Terrain(){
-	for (int i=0; i<terrain_y; ++i){
-		for (int j=0; j<terrain_x; ++j){
-
+void DrawMap(){
+	for (int i=0; i<terrain_x; ++i){
+		for (int j=0; j<terrain_y; ++j){
+			std::putchar(get_terrain_tile(i,j));
+/*
 			switch ( get_terrain_tile(i,j)){
 				case 'a':
-					std::putchar('a');
 					break;
 				case 'b':
-					std::putchar('b');
 					break;
 				case 'c':
-					std::putchar('c');
 					break;
 				case 'd':
-					std::putchar('d');
 					break;
 				case 'e':
-					std::putchar('e');
 					break;
 				case 'f':
-					std::putchar('f');
 					break;
 				case 'g':
-					std::putchar('g');
 					break;
 				case 'h':
-					std::putchar('h');
 					break;
 				case 'i':
-					std::putchar('i');
 					break;
 				case 'j':
-					std::putchar('j');
 					break;
 				case 'k':
-					std::putchar('k');
 					break;
 				case 'l':
-					std::putchar('l');
 					break;
 				default:
 					std::puts("Unrecognized tile, program terminated.");
@@ -83,13 +74,29 @@ void Draw_Terrain(){
 					break;
 
 			}
-		}
+*/		}
 		std::putchar('\n');
 	}
 }
 
-void run_terrain_test(){
-	Load_Terrain_Text(STAGE1_PATH);
-	Draw_Terrain();
+int main(int argc, char **argv){
+
+	ALLEGRO_DISPLAY *display;
+	bool done = false;
+
+	std::string terrain_data_path = TERRAIN__DATA_PATH;
+	terrain_data_path.append(STAGE1);
+	Load_Terrain(terrain_data_path);
+	
+	terrain_data_path = TERRAIN__DATA_PATH;
+/*
+	if (!al_init()){
+		al_show_native_message_box(NULL,"Error", NULL, "Could not initialize Allegro", NULL, NULL);
+	}
+
+	if ( (display = al_create_display(SCREEN_W, SCREEN_H)) == NULL){
+	}
+*/
+	DrawMap();
 	getchar();
 }
