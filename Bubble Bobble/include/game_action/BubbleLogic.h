57b6a395a31c6a9a6e795c00b1b5cb226a047a51
Angel Kyriakopoulos <angelkyriako@gmail.com>
2013-02-01 00:24:12 +0200

    practicaly nothing

---
+++ Bubble Bobble/include/game_action/BubbleLogic.h
@@ -0,0 +1,18 @@
+#ifndef BUBBLELOGIC_H
+#define BUBBLELOGIC_H
+
+typedef unsigned int Points;
+typedef unsigned int Lifes;
+
+enum Fruit{ Banana, Peach, Orange, WaterMelon}
+enum PowerUp{ RedShoes, YellowSweet, BlueSweet, PurpleSweet}
+
+class BubbleLogic{
+
+	private:
+		Lifes BubLifes;
+		Points[] points;
+		bool[] PowerUps;
+}
+
+#endif
